1e9bdcfbee280ea94a149eb4c0d2c883cc83fe7e
Magesh <tkmagesh77@gmail.com>
2013-06-11 15:57:00 +0530

    WCF Demo

---
+++ Day-2/WCF/Service/SchneiderServices/SchneiderServices.Host/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using SchneiderServices.Contracts;
+using SchneiderServices.Implementations;
+
+namespace SchneiderServices.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new ServiceHost(typeof (CalculatorService), new Uri[] {});
+            
+            host.AddServiceEndpoint(typeof (ICalculator), new BasicHttpBinding(),
+                                    "http://localhost:9090/SchneiderServices/CalculatorService");
+            host.Open();
+            foreach (var endpoint in host.Description.Endpoints)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}",endpoint.Contract.Name, endpoint.Address.Uri, endpoint.Binding.Name );
+            }
+            Console.WriteLine("Hit ENTER to shutdown...");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
---
+++ Day-2/WCF/Service/SchneiderServices/SchneiderServices.Implementations/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System.ServiceModel;
+using SchneiderServices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchneiderServices.Implementations
+{
+    [ServiceBehavior]
+    public class CalculatorService : ICalculator
+    {
+        [OperationBehavior]
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        [OperationBehavior]
+        public int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        [OperationBehavior]
+        public int Multiply(int number1, int number2)
+        {
+            return number1*number2;
+        }
+
+        [OperationBehavior]
+        public int Divide(int number1, int number2)
+        {
+            return number1/number2;
+        }
+    }
+}
